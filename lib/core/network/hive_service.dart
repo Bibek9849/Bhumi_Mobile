d4d88f572267663070a22c47c4f969edbb6b6ef1
Bibek Pandey <pandeybibe098k@gmail.com>
2025-01-17 19:04:34 +0545

    added hive service

--- lib/core/network/hive_service.dart
+++ lib/core/network/hive_service.dart
@@ -1,1 +1,51 @@
+import 'package:bhumi_mobile/app/constants/hive_table_constant.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
+import '../../features/auth/data/model/auth_hive_model.dart';
+
+class HiveService {
+  static Future<void> init() async {
+    // Initialize the database
+    var directory = await getApplicationDocumentsDirectory();
+    var path = '${directory.path}Bhumi.db';
+
+    Hive.init(path);
+  }
+
+  // Auth Queries
+  Future<void> register(AuthHiveModel auth) async {
+    var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.studentBox);
+    await box.put(auth.studentId, auth);
+  }
+
+  Future<void> deleteAuth(String id) async {
+    var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.studentBox);
+    await box.delete(id);
+  }
+
+  Future<List<AuthHiveModel>> getAllAuth() async {
+    var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.studentBox);
+    return box.values.toList();
+  }
+
+  // Login using email and password
+  Future<AuthHiveModel?> login(String contact, String password) async {
+    var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.studentBox);
+    var auth = box.values.firstWhere(
+      (element) =>
+          element.contact == contact &&
+          element.password == password, // Using email for comparison
+      orElse: () => const AuthHiveModel.initial(),
+    );
+    return auth;
+  }
+
+  Future<void> clearAll() async {
+    await Hive.deleteBoxFromDisk(HiveTableConstant.studentBox);
+  }
+
+  Future<void> close() async {
+    await Hive.close();
+  }
+}
