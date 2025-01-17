f7697fa263d804b01409115a1af455548b4caf51
Bibek Pandey <pandeybibe098k@gmail.com>
2025-01-17 19:01:26 +0545

    updated hivetable, di and usecase

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,3 +1,10 @@
+import 'package:bhumi_mobile/core/network/hive_service.dart';
+import 'package:bhumi_mobile/features/auth/data/data_source/local_data_source/auth_local_data_source.dart';
+import 'package:bhumi_mobile/features/auth/data/repository/auth_local_repository.dart';
+import 'package:bhumi_mobile/features/auth/domain/use_case/login_use_case.dart';
+import 'package:bhumi_mobile/features/auth/domain/use_case/register_user_usecase.dart';
+import 'package:bhumi_mobile/features/auth/presentation/view_model/bloc/login_bloc.dart';
+import 'package:bhumi_mobile/features/auth/presentation/view_model/bloc/signup_bloc.dart';
 import 'package:bhumi_mobile/features/onboarding/presentation/view_model/onboarding_cubit.dart';
 import 'package:bhumi_mobile/features/splash/presentation/view_model/splash_cubit.dart';
 import 'package:get_it/get_it.dart';
@@ -5,18 +12,65 @@ import 'package:get_it/get_it.dart';
 final getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
-  await _initSplashScreenDependencies();
-  await _initOnboardingScreenDependencies();
+  // First initialize hive service
+  await _initHiveService();
+
+  // Initialize the onboarding cubit and other dependencies
+  await _initSplashDependencies();
+
+  await _initOnboardingDependencies();
+  await _initRegisterDependencies();
+  await _initLoginDependencies();
 }
 
-Future<void> _initSplashScreenDependencies() async {
-  getIt.registerFactory<SplashCubit>(
-    () => SplashCubit(),
+_initHiveService() {
+  getIt.registerLazySingleton<HiveService>(() => HiveService());
+}
+
+_initSplashDependencies() {
+  getIt.registerFactory<SplashCubit>(() => SplashCubit());
+}
+
+_initOnboardingDependencies() {
+  getIt.registerFactory<OnboardingCubit>(() => OnboardingCubit());
+}
+
+_initRegisterDependencies() {
+  // init local data source
+  getIt.registerLazySingleton(
+    () => AuthLocalDataSource(getIt<HiveService>()),
+  );
+
+  // init local repository
+  getIt.registerLazySingleton(
+    () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
+  );
+
+  // register use usecase
+  getIt.registerLazySingleton<RegisterUserUseCase>(
+    () => RegisterUserUseCase(
+      getIt<AuthLocalRepository>(),
+    ),
+  );
+
+  getIt.registerFactory<SignupBloc>(
+    () => SignupBloc(
+      registerUseCase: getIt(),
+    ),
   );
 }
 
-Future<void> _initOnboardingScreenDependencies() async {
-  getIt.registerFactory<OnboardingCubit>(
-    () => OnboardingCubit(),
+_initLoginDependencies() async {
+  getIt.registerLazySingleton<LoginUseCase>(
+    () => LoginUseCase(
+      getIt<AuthLocalRepository>(),
+    ),
+  );
+
+  getIt.registerFactory<LoginBloc>(
+    () => LoginBloc(
+      signupBloc: getIt<SignupBloc>(),
+      loginUseCase: getIt<LoginUseCase>(),
+    ),
   );
 }
