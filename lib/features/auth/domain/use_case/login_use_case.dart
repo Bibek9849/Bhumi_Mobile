145c3dfb3f871874df16bd146e955af85300b2f0
Bibek Pandey <pandeybibe098k@gmail.com>
2025-01-31 16:19:48 +0545

    created usecase for uploading image

--- lib/features/auth/domain/use_case/login_use_case.dart
+++ lib/features/auth/domain/use_case/login_use_case.dart
@@ -1,3 +1,4 @@
+import 'package:bhumi_mobile/app/shared_prefs/token_shared_prefs.dart';
 import 'package:bhumi_mobile/app/usecase/usecase.dart';
 import 'package:bhumi_mobile/core/error/failure.dart';
 import 'package:bhumi_mobile/features/auth/domain/repository/auth_repository.dart';
@@ -24,11 +25,26 @@ class LoginParams extends Equatable {
 
 class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
   final IAuthRepository repository;
+  final TokenSharedPrefs tokenSharedPrefs;
 
-  LoginUseCase(this.repository);
+  LoginUseCase(this.repository, this.tokenSharedPrefs);
 
   @override
   Future<Either<Failure, String>> call(LoginParams params) {
-    return repository.loginStudent(params.contact, params.password);
+    // Save token in Shared Preferences
+    return repository
+        .loginStudent(params.contact, params.password)
+        .then((value) {
+      return value.fold(
+        (failure) => Left(failure),
+        (token) {
+          tokenSharedPrefs.saveToken(token);
+          tokenSharedPrefs.getToken().then((value) {
+            print(value);
+          });
+          return Right(token);
+        },
+      );
+    });
   }
 }
