72c80320b9a643ccb0cbcf4e47b3b84e30b4702f
Bibek Pandey <pandeybibe098k@gmail.com>
2025-02-07 14:57:07 +0545

    created repo.mock file to implement AuhRepo

---
+++ test/features/auth/domain/usecase/repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:bhumi_mobile/features/auth/domain/repository/auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements IAuthRepository {}
